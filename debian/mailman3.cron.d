40fdfab71732690188e65a2eb60fbcbf2beab8c6
Jonas Meurer <jonas@freesources.org>
2020-12-29 13:32:09 +0100

    d/mailman3.cron.d: Add cronjob for gating messages from usenet to mailinglists

--- debian/mailman3.cron.d
+++ debian/mailman3.cron.d
@@ -6,5 +6,8 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 # At 8AM, send out notifices of pending requests to list moderators
 0  8 * * *  list	if [ -x /usr/bin/mailman ]; then /usr/bin/mailman notify; fi
 
-# At 12AM, send mail digests for lists that do periodic as well as threshhold delivery
+# At 12AM, send mail digests for lists that do periodic as well as threshold delivery
 0 12 * * *  list	if [ -x /usr/bin/mailman ]; then /usr/bin/mailman digests --periodic; fi
+
+# Every 15 minutes, gate messages from usenet to those lists which have the gateway configured
+*/15 * * * *   list    if [ -x /usr/bin/mailman ]; then /usr/bin/mailman gatenews; fi
